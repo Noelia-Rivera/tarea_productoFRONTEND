5449023c32d429c5a0eb8c1a339cf4058d244d45
Marquitoz <markst1811@gmail.com>
2024-11-07 18:45:55 -0500

    ultimos cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,16 @@
 import { Routes } from '@angular/router';
 import { ProductoComponent } from './componentes/producto/producto.component';
 import { CategoriaComponent } from './componentes/categoria/categoria.component';
-<<<<<<< HEAD
-=======
 import { HomeComponent } from './home/home.component';
->>>>>>> dc4285e (commit)
 
 export const routes: Routes = [
     {
         path: '',
-<<<<<<< HEAD
-=======
         component: HomeComponent,
         title: 'home'
     },
     {
         path: 'producto',
->>>>>>> dc4285e (commit)
         component: ProductoComponent,
         title: 'Producto'
     },
